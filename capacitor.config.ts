c3a0912079c16755d8b1e3b6c3dc5c54cd7c90e1
julien <cocomimi.games@gmail.com>
2026-08-24 22:11:56 +0200

    init + ci/cd github pages

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Dungeon Crawler Carl Book Companion',
+  webDir: 'dist'
+};
+
+export default config;
